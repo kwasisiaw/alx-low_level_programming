07c7f065124f2751d6dd070b81f8dd55a47373b0
kwasisiaw <kwasisterling007@gmail.com>
2022-09-21 10:01:33 -0700

    23

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,11 @@
 
 /**
  * This function only prints out something
+ * It really doesn't do much 
  * Return: It returns nothing
  */
 int main(void)
 {
-	
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-
 }
